92e82e40c5b5651f4ed481859f31057811dbfc0f
Carlos <pruebasdesarrollo21@gmail.com>
2020-03-31 13:59:14 -0500

    add capas

---
+++ pulsacionesdonet/Controllers/EjemploController.cs
@@ -0,0 +1,13 @@
+namespace pulsacionesdonet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EjemploController
+    {
+        public EjemploController(){
+
+        }
+
+        public int MiAtributo { get; set}
+    }
+}
